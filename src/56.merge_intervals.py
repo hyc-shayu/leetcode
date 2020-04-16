63be7a26788b0bbecce0f717a56615a5fe59a8fe
hyc <hyc_email555@163.com>
2020-04-16 21:55:55 +0800

    每日一题,合并区间

---
+++ src/56.merge_intervals.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+
+        intervals.sort()
+
+        res = [intervals[0]]
+        for interval in intervals[1:]:
+            if interval[0] > res[-1][1]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+
+        return res
