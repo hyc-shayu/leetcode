e275eeaf3d46cf1595cb9c9bb4174813f08a91b0
hyc <hyc_email555@163.com>
2020-05-09 22:28:37 +0800

    add api string_to_tree_node

--- src/tool/test_data.py
+++ src/tool/test_data.py
@@ -1,4 +1,4 @@
-from src.tool.struct import ListNode
+from src.tool.struct import ListNode, TreeNode
 import random
 
 _defaultLen = 5
@@ -33,3 +33,37 @@ def get_sorted_node_list(length=_defaultLen, min_val=_min_val, max_val=_max_val)
         cur.next = ListNode(v)
         cur = cur.next
     return head.next
+
+
+def string_to_tree_node(input_):
+    input_ = input_.strip()
+    input_ = input_[1:-1]
+    if not input_:
+        return None
+
+    input_values = [s.strip() for s in input_.split(',')]
+    root = TreeNode(int(input_values[0]))
+    node_queue = [root]
+    front = 0
+    index = 1
+    while index < len(input_values):
+        node = node_queue[front]
+        front = front + 1
+
+        item = input_values[index]
+        index = index + 1
+        if item != "null":
+            left_number = int(item)
+            node.left = TreeNode(left_number)
+            node_queue.append(node.left)
+
+        if index >= len(input_values):
+            break
+
+        item = input_values[index]
+        index = index + 1
+        if item != "null":
+            right_number = int(item)
+            node.right = TreeNode(right_number)
+            node_queue.append(node.right)
+    return root
